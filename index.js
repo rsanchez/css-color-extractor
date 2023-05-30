782ef5665e62a2a051c212211a435562994b1ff6
Rob Sanchez <rob@robsanchez.com>
2023-05-29 20:28:15 -0400

    Update tooling

--- index.js
+++ index.js
@@ -1,152 +1,166 @@
-'use strict';
+'use strict'
 
-module.exports = new CssColorExtractor();
+const postcss = require('postcss')
+const unique = require('array-unique')
+const Color = require('color')
 
-function CssColorExtractor() {
-    var postcss = require('postcss');
-    var util = require('util');
-    var unique = require('array-unique');
-    var Color = require('color');
-    var propertiesWithColors = [
-        'color',
-        'background',
-        'background-color',
-        'background-image',
-        'border',
-        'border-top',
-        'border-right',
-        'border-bottom',
-        'border-left',
-        'border-color',
-        'border-top-color',
-        'border-right-color',
-        'border-bottom-color',
-        'border-left-color',
-        'outline',
-        'outline-color',
-        'text-shadow',
-        'box-shadow',
-        'fill',
-        'stroke',
-        'stop-color',
-        'flood-color',
-        'lighting-color'
-    ];
-    var colorFormats = [
-        'hexString',
-        'rgbString',
-        'percentString',
-        'hslString',
-        'hwbString',
-        'keyword'
-    ];
+function CssColorExtractor () {
+  const propertiesWithColors = [
+    'color',
+    'background',
+    'background-color',
+    'background-image',
+    'border',
+    'border-top',
+    'border-right',
+    'border-bottom',
+    'border-left',
+    'border-color',
+    'border-top-color',
+    'border-right-color',
+    'border-bottom-color',
+    'border-left-color',
+    'outline',
+    'outline-color',
+    'text-shadow',
+    'box-shadow',
+    'fill',
+    'stroke',
+    'stop-color',
+    'flood-color',
+    'lighting-color'
+  ]
+  const colorFormats = [
+    'hexString',
+    'rgbString',
+    'percentString',
+    'hslString',
+    'hwbString',
+    'keyword',
+  ]
 
-    function doesPropertyAllowColor(property) {
-        return propertiesWithColors.indexOf(property) > -1;
-    }
+  function doesPropertyAllowColor (property) {
+    return propertiesWithColors.indexOf(property) > -1
+  }
 
-    function isColorGrey(color) {
-        var red = color.red();
+  function isColorGrey (color) {
+    const red = color.red()
 
-        // we only need to test one color
-        // since isColorMonochrome assures that all
-        // three rgb colors are equal
+    // we only need to test one color
+    // since isColorMonochrome assures that all
+    // three rgb colors are equal
 
-        return isColorMonochrome(color) && red > 0 && red < 255;
-    }
+    return isColorMonochrome(color) && red > 0 && red < 255
+  }
 
-    function isColorMonochrome(color) {
-        var hsl = color.hsl();
+  function isColorMonochrome (color) {
+    const hsl = color.hsl().object()
 
-        return hsl.h === 0 && hsl.s === 0;
-    }
+    return hsl.h === 0 && hsl.s === 0
+  }
 
-    function isValidColorFormat(format) {
-        return colorFormats.indexOf(format) > -1;
-    }
+  function isValidColorFormat (format) {
+    return colorFormats.indexOf(format) > -1
+  }
 
-    function extractColors(string, options) {
-        var colors = [];
-        var values = [];
+  function extractColors (string, options) {
+    const colors = []
+    let values = []
 
-        options = util._extend({
-            withoutGrey:       false,
-            withoutMonochrome: false,
-            colorFormat:       null
-        }, options);
+    options = Object.assign({
+      withoutGrey: false,
+      withoutMonochrome: false,
+      colorFormat: null
+    }, options)
 
-        postcss.list.comma(string).forEach(function (items) {
-            postcss.list.space(items).forEach(function (item) {
-                var regex = new RegExp(
-                    '^' +
+    postcss.list.comma(string).forEach(function (items) {
+      postcss.list.space(items).forEach(function (item) {
+        const regex = new RegExp(
+          '^' +
                     '(-webkit-|-moz-|-o-)?' +
                     '(repeating-)?' +
                     '(radial|linear)-gradient\\((.*?)\\)' +
                     '$'
-                );
+        )
 
-                var match = item.match(regex);
+        const match = item.match(regex)
 
-                if (match) {
-                    values = values.concat.apply(
-                        values,
-                        postcss.list.comma(match[4]).map(postcss.list.space)
-                    );
-                } else {
-                    values.push(item);
-                }
-            });
-        });
-
-        values.forEach(function (value) {
-            var color;
-
-            // check if it is a valid color
-            try {
-                color = new Color(value);
-            } catch (e) {
-                return;
-            }
-
-            if (options.withoutMonochrome && isColorMonochrome(color)) {
-                return;
-            }
-
-            if (options.withoutGrey && isColorGrey(color)) {
-                return;
-            }
-
-            if (isValidColorFormat(options.colorFormat)) {
-                value = color[options.colorFormat]();
-            }
-
-            if (value) {
-                colors.push(value);
-            }
-        });
-
-        return unique(colors);
-    }
-
-    function extractColorsFromDecl(decl, options) {
-        if (!doesPropertyAllowColor(decl.prop)) {
-            return [];
+        if (match) {
+          values = values.concat.apply(
+            values,
+            postcss.list.comma(match[4]).map(postcss.list.space)
+          )
+        } else {
+          values.push(item)
         }
+      })
+    })
 
-        return extractColors(decl.value, options);
+    values.forEach(function (value) {
+      let color
+
+      // check if it is a valid color
+      try {
+        color = new Color(value)
+      } catch (e) {
+        return
+      }
+
+      if (options.withoutMonochrome && isColorMonochrome(color)) {
+        return
+      }
+
+      if (options.withoutGrey && isColorGrey(color)) {
+        return
+      }
+
+      if (options.colorFormat === 'keyword') {
+        value = color.keyword()
+        // convert back to rgb to see if keyword is an exact match
+        const keywordColor = new Color(value)
+        if (keywordColor.rgbNumber() !== color.rgbNumber()) {
+          return
+        }
+      } else if (isValidColorFormat(options.colorFormat)) {
+        let colorFormat = options.colorFormat
+        if (!color[options.colorFormat]) {
+          colorFormat = colorFormat.replace(/String$/, '')
+        }
+        value = color[colorFormat]()
+        if (typeof value !== 'string') {
+          value = value.string()
+        }
+      }
+
+      if (value) {
+        colors.push(value)
+      }
+    })
+
+    return unique(colors)
+  }
+
+  function extractColorsFromDecl (decl, options) {
+    if (!doesPropertyAllowColor(decl.prop)) {
+      return []
     }
 
-    function extractColorsFromCss(css, options) {
-        var colors = [];
+    return extractColors(decl.value, options)
+  }
 
-        postcss.parse(css).walkDecls(function (decl) {
-            colors = colors.concat(extractColorsFromDecl(decl, options));
-        });
+  function extractColorsFromCss (css, options) {
+    let colors = []
 
-        return unique(colors);
-    }
+    postcss.parse(css).walkDecls(function (decl) {
+      colors = colors.concat(extractColorsFromDecl(decl, options))
+    })
 
-    this.fromDecl = extractColorsFromDecl;
-    this.fromCss = extractColorsFromCss;
-    this.fromString = extractColors;
+    return unique(colors)
+  }
+
+  this.fromDecl = extractColorsFromDecl
+  this.fromCss = extractColorsFromCss
+  this.fromString = extractColors
 }
+
+module.exports = new CssColorExtractor()
